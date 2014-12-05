eba1dbafe9b5729874fda870399ae66e9b0bc8db
Markus Kauppila <markus.kauppila@gmail.com>
2014-12-05 21:30:11 +0200

    Mock up the API

---
+++ CCHCache/CCHCache.h
@@ -0,0 +1,35 @@
+//
+//  CCHCache.h
+//  CCHCache
+//
+//  Created by Markus Kauppila on 05/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^CCHCacheCompletionBlock)(NSString *key, id <NSCopying> value);
+
+@interface CCHCache : NSObject
+
+/// Name of the cache. Also used as cache directory name.
+@property (nonatomic, copy, readonly) NSString *name;
+
+- (instancetype)initWithInMemorySize:(NSUInteger)sizeInBytes
+                         andDiskSize:(NSUInteger)sizeInBytes
+                             forName:(NSString *)name NS_DESIGNATED_INITIALIZER;
+
+/// Retrieve object for `key`
+- (id<NSCopying>)objectForKey:(NSString *)key
+               withCompletion:(CCHCacheCompletionBlock)completion;
+
+/// Store `object` for a `key`
+- (void)setObject:(id <NSCopying>)object
+           forKey:(NSString *)key
+   withCompletion:(CCHCacheCompletionBlock)completion;
+
+/// Remove object for `key`
+- (void)removeObjectForKey:(NSString *)key
+            withCompletion:(CCHCacheCompletionBlock)completion;
+
+@end
---
+++ CCHCache/CCHCache.m
@@ -0,0 +1,51 @@
+//
+//  CCHCache.m
+//  CCHCache
+//
+//  Created by Markus Kauppila on 05/12/14.
+//
+//
+
+#import "CCHCache.h"
+
+@interface CCHCache ()
+
+@property (nonatomic, assign, readonly) NSUInteger memoryCacheSizeInBytes;
+@property (nonatomic, assign, readonly) NSUInteger diskCacheSizeInBytes;
+
+@end
+
+@implementation CCHCache
+
+- (instancetype)initWithInMemorySize:(NSUInteger)memoryCacheSizeInBytes
+                         andDiskSize:(NSUInteger)diskCacheSizeInBytes
+                             forName:(NSString *)name
+{
+    self = [super init];
+    if (self == nil) return nil;
+
+    _name = name;
+    _memoryCacheSizeInBytes = memoryCacheSizeInBytes;
+    _diskCacheSizeInBytes = diskCacheSizeInBytes;
+
+    return self;
+}
+
+- (id<NSCopying>)objectForKey:(__unused NSString *)key
+               withCompletion:(__unused CCHCacheCompletionBlock)completion
+{
+    return nil;
+}
+
+- (void)setObject:(__unused id <NSCopying>)object
+           forKey:(__unused NSString *)key
+   withCompletion:(__unused CCHCacheCompletionBlock)completion
+{
+}
+
+- (void)removeObjectForKey:(__unused NSString *)key
+            withCompletion:(__unused CCHCacheCompletionBlock)completion
+{
+}
+
+@end
