79c3b1a5f6878cd159775c8b9eba58a0c66c08cc
Markus Kauppila <markus.kauppila@gmail.com>
2014-12-07 20:00:34 +0200

    Add memory cache

---
+++ CCHMemoryCache.h
@@ -0,0 +1,13 @@
+//
+//  CCHMemoryCache.h
+//  CCHCache
+//
+//  Created by Markus Kauppila on 07/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCHMemoryCache : NSObject
+
+@end
---
+++ CCHMemoryCache.m
@@ -0,0 +1,13 @@
+//
+//  CCHMemoryCache.m
+//  CCHCache
+//
+//  Created by Markus Kauppila on 07/12/14.
+//
+//
+
+#import "CCHMemoryCache.h"
+
+@implementation CCHMemoryCache
+
+@end
