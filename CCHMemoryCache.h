0ea98fb78875ed2e7141f63bd92232e754876e2b
Markus Kauppila <markus.kauppila@gmail.com>
2014-12-07 20:22:07 +0200

    Add skeleton implementation of memory cache

--- CCHMemoryCache.h
+++ CCHMemoryCache.h
@@ -8,6 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+#import "CCHCache.h"
+
 @interface CCHMemoryCache : NSObject
+
+- (instancetype)initWithSize:(NSUInteger)sizeInBytes
+                    withName:(NSString *)name;
+
+#pragma mark - Public API
+
+- (id<NSCopying>)objectForKey:(NSString *)key
+               withCompletion:(CCHCacheCompletionBlock)completion;
+
+- (void)setObject:(id <NSCopying>)object
+           forKey:(NSString *)key
+   withCompletion:(CCHCacheCompletionBlock)completion;
+
+- (void)removeObjectForKey:(NSString *)key
+            withCompletion:(CCHCacheCompletionBlock)completion;
 
 @end
--- CCHMemoryCache.m
+++ CCHMemoryCache.m
@@ -8,6 +8,45 @@
 
 #import "CCHMemoryCache.h"
 
+@interface CCHMemoryCache ()
+
+@property (nonatomic, readonly) NSUInteger sizeInBytes;
+@property (nonatomic, copy, readonly) NSString *name;
+
+@end
+
 @implementation CCHMemoryCache
+
+- (instancetype)initWithSize:(NSUInteger)sizeInBytes
+                    withName:(NSString *)name;
+{
+    self = [super init];
+    if (self == nil) return nil;
+
+
+    _sizeInBytes = sizeInBytes;
+    _name = name;
+
+    return self;
+}
+
+- (id<NSCopying>)objectForKey:(NSString *)key
+               withCompletion:(CCHCacheCompletionBlock)completion
+{
+
+}
+
+- (void)setObject:(id <NSCopying>)object
+           forKey:(NSString *)key
+   withCompletion:(CCHCacheCompletionBlock)completion
+{
+
+}
+
+- (void)removeObjectForKey:(NSString *)key
+            withCompletion:(CCHCacheCompletionBlock)completion
+{
+
+}
 
 @end
