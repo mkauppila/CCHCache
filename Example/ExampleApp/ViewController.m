58096143bc687406fd30ea7dd36c0e115a3a906d
Markus Kauppila <markus.kauppila@gmail.com>
2015-02-25 20:57:01 +0200

    Implement tiny test in example app

--- Example/ExampleApp/ViewController.m
+++ Example/ExampleApp/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
+#import "CCHCache.h"
+
 @interface ViewController ()
+
+@property (nonatomic, strong) CCHDiskCache *cache;
 
 @end
 
@@ -16,7 +20,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.cache = [[CCHDiskCache alloc] initWithSize:5000000 withName:@"number-cache"];
+    [self.cache setObject:@1001 forKey:@"one" withCompletion:^(NSString *key, id <NSCopying> value) {
+        NSLog(@"set value %@ for key %@", value, key);
+    }];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    [self.cache objectForKey:@"one" withCompletion:^(NSString *key, id <NSCopying> value) {
+        NSLog(@"%@: %@", key, value);
+    }];
+
+    [self.cache removeObjectForKey:@"one" withCompletion:^(NSString *key, id <NSCopying> value) {
+        NSLog(@"Deleted key %@ of value %@", key, value);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
